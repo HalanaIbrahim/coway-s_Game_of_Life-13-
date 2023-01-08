96ba844605818e9d658bffca8e009e0c6407a0d5
Ibrahim Halana <halanaibrahim@gmail.com>
2023-01-07 19:50:16 -0500

    for and while loops fixed!

--- main.py
+++ main.py
@@ -1,22 +1,38 @@
 import copy, random, sys, time
 
 #cell grid 
-WIDTH = '56'
-HEIGHT = '24'
+WIDTH = 79
+HEIGHT = 24
 
 #game characters 
 
-ALIVE = 0 
-DEAD = ' '
+ALIVE = '0'
+DEAD = '-'
 
 #empty dictionary 
 nextCell = {}
 
 #loop that randomly assings the characters 
-for x in WIDTH:
-    for y in HEIGHT:
-        random.randint(0,1)
+for x in range(WIDTH):
+    for y in range(HEIGHT):
+        if random.randint(0,1) == 0:
+            nextCell [(x, y)] = ALIVE
+        else:
+            nextCell [(x, y)] = DEAD
+    
 
-while TRUE:
+while True:
     print('\n * 50')
+    cell = copy.deepcopy(nextCell)
+    for y in range(HEIGHT):
+        for x in range(WIDTH):
+            print(cell[(x,y)], end=' ')
+    
+    for x in range(WIDTH):
+        for y in range(HEIGHT):
+            left = (x - 1) % WIDTH
+            right = (x + 1) % WIDTH
+            above = (y - 1) % HEIGHT
+            below = (y + 1) % HEIGHT
+    aliveNe = 0
     
