b2a8af8425cbd4ad4d4a5fb910772bb3dafe5221
Ibrahim Halana <halanaibrahim@gmail.com>
2023-01-07 19:42:35 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,22 @@
+import copy, random, sys, time
+
+#cell grid 
+WIDTH = '56'
+HEIGHT = '24'
+
+#game characters 
+
+ALIVE = 0 
+DEAD = ' '
+
+#empty dictionary 
+nextCell = {}
+
+#loop that randomly assings the characters 
+for x in WIDTH:
+    for y in HEIGHT:
+        random.randint(0,1)
+
+while TRUE:
+    print('\n * 50')
+    
